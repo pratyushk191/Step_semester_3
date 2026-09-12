871df26bb95de7b78be995fa6f62ad242da789a3
pratyushk191 <pratyushk191@gmail.com>
2026-09-12 16:25:12 +0530

    Add Session 2: String Operations & Performance

---
+++ src/main/java/String Operations & Performance/class_problems/FileExtensionValidator.java
@@ -0,0 +1,31 @@
+public class FileExtensionValidator {
+
+    static String validateFileExtension(String filename) {
+
+        int dot = filename.lastIndexOf('.');
+
+        if (dot == -1) {
+            return "Rejected — invalid file type";
+        }
+
+        String extension = filename.substring(dot + 1);
+
+        if (extension.equalsIgnoreCase("pdf") ||
+            extension.equalsIgnoreCase("docx") ||
+            extension.equalsIgnoreCase("zip")) {
+
+            return "Accepted";
+        }
+
+        return "Rejected — invalid file type";
+    }
+
+    public static void main(String[] args) {
+
+        String filename = "Assignment1.PDF";
+
+        String result = validateFileExtension(filename);
+
+        System.out.println(result);
+    }
+}
